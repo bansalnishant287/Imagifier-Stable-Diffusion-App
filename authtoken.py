16b05dcbe0db0d34934b6fc2412023c6bdc6ed83
Nishant Bansal <nishant.1923it1002@kiet.edu>
2022-09-27 21:42:56 +0530

    Add files via upload

---
+++ authtoken.py
@@ -0,0 +1,3 @@
+auth_token = 'YOUR HUGGING FACE TOKEN HERE'
+# Although it is public library you still need to accept the Terms & Conditions 
+# How to get one: https://huggingface.co/docs/hub/security-tokens
